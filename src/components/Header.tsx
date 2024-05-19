fe2c133d9c657a15ffbe4af79339d1f67bfada04
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-19 10:35:44 -0300

    fix: header height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ interface HeaderProps {
 
 export function Header({}: HeaderProps) {
   return (
-    <div className="py-4 px-4 shadow flex justify-center">
+    <div className="py-2 px-4 shadow flex justify-center">
       <Link href={'/'} className="text-2xl text-neutral font-black">
         vítreo<span className="text-accent">.in</span>
       </Link>
