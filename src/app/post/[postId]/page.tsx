95b319f8fdb7dbc9249a5871c4fe3759e710ada2
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-19 14:52:50 -0300

    feat: add images and files

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -1,4 +1,7 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import { FaDownload } from 'react-icons/fa6';
 
 import { prisma } from '@/libs/prisma';
 
@@ -56,14 +59,31 @@ export default async function Post({
       title: true,
       description: true,
       content: true,
+      image: true,
     },
   });
 
   if (!post) notFound();
   return (
     <div>
-      <h1 className="text-4xl font-black">{post.title}</h1>
-      <h2 className="opacity-50 text-2xl mb-8">{post.description}</h2>
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-8 gap-4">
+        <div>
+          <h1 className="text-4xl font-black">{post.title}</h1>
+          <h2 className="opacity-50 text-2xl ">{post.description}</h2>
+        </div>
+        <Link className="btn btn-accent btn-sm" href={'/files/olho-seco-1.pdf'} target="_blank">
+          <FaDownload className="h-4 w-4" />
+          Baixar
+        </Link>
+      </div>
+      <Image
+        src={post.image}
+        alt={post.title}
+        width={500}
+        height={400}
+        className="mx-auto"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+      />
       <div className="prose">
         <div
           dangerouslySetInnerHTML={{
