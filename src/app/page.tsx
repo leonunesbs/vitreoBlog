2c7a3bd8bac1d1a1b1d316f19454b6283f8e15c0
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-19 15:27:45 -0300

    feat: add middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,3 @@
-import { CategoriesBar, PostCard } from '@/components';
-import { prisma } from '@/libs/prisma';
-
 export default async function Home() {
-  const posts = await prisma.post.findMany({
-    include: {
-      categories: true,
-    },
-    where: {
-      published: true,
-    },
-  });
-  return (
-    <div className="max-w-2xl mx-auto py-12 px-4 space-y-4">
-      <CategoriesBar />
-      {posts.map((post) => (
-        <div key={post.id}>
-          <PostCard post={post} />
-          <div className="divider mb-8"></div>
-        </div>
-      ))}
-    </div>
-  );
+  return <div className="max-w-2xl mx-auto py-12 px-4 space-y-4">Redirect</div>;
 }
