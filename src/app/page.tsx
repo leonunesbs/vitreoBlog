81cbd1ae6f7e9287c39e92cf6597df8bc6a37576
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-19 15:25:32 -0300

    fix: fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import { redirect } from 'next/navigation';
+import { CategoriesBar, PostCard } from '@/components';
+import { prisma } from '@/libs/prisma';
 
 export default async function Home() {
-  redirect('/feed');
+  const posts = await prisma.post.findMany({
+    include: {
+      categories: true,
+    },
+    where: {
+      published: true,
+    },
+  });
+  return (
+    <div className="max-w-2xl mx-auto py-12 px-4 space-y-4">
+      <CategoriesBar />
+      {posts.map((post) => (
+        <div key={post.id}>
+          <PostCard post={post} />
+          <div className="divider mb-8"></div>
+        </div>
+      ))}
+    </div>
+  );
 }
