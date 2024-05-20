16cb0b90d28cfd381da5dd4f9c41eff7326d1a4c
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-19 22:17:21 -0300

    fix: fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,10 @@
 import dynamic from 'next/dynamic';
 
-const HankoAuth = dynamic(() => import('@/components/HankoAuth'), { ssr: false });
+const HankoAuth = dynamic(() => import('@/components/HankoAuth').then((mod) => mod.HankoAuth), { ssr: false });
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center">
+    <div>
       <HankoAuth />
     </div>
   );
