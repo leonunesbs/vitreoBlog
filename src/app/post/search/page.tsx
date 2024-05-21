c8b642fcce91f10c482fb8346a208400c48bef5f
Leonardo Nunes <leonunesbs@gmail.com>
2024-05-20 21:43:56 -0300

    fix: fix

--- src/app/post/search/page.tsx
+++ src/app/post/search/page.tsx
@@ -1,6 +1,3 @@
-import { CategoriesBar, PostCard } from '@/components';
-import { prisma } from '@/libs/prisma';
-
 export default async function Search({
   searchParams,
 }: {
@@ -8,41 +5,43 @@ export default async function Search({
     q: string;
   };
 }) {
-  const searchString = searchParams?.q;
-  const criteria = {
-    contains: searchString,
-  };
+  // const searchString = searchParams?.q;
+  // const criteria = {
+  //   contains: searchString,
+  // };
 
-  const posts = await prisma.post.findMany({
-    where: {
-      OR: [
-        {
-          title: criteria,
-        },
-        {
-          description: criteria,
-        },
-        {
-          categories: {
-            some: {
-              name: criteria,
-            },
-          },
-        },
-        {
-          content: criteria,
-        },
-      ],
-      published: true,
-    },
-    include: {
-      categories: true,
-    },
-  });
+  console.log(searchParams?.q);
+
+  // const posts = await prisma.post.findMany({
+  //   where: {
+  //     OR: [
+  //       {
+  //         title: criteria,
+  //       },
+  //       {
+  //         description: criteria,
+  //       },
+  //       {
+  //         categories: {
+  //           some: {
+  //             name: criteria,
+  //           },
+  //         },
+  //       },
+  //       {
+  //         content: criteria,
+  //       },
+  //     ],
+  //     published: true,
+  //   },
+  //   include: {
+  //     categories: true,
+  //   },
+  // });
 
   return (
     <>
-      <CategoriesBar />
+      {/* <CategoriesBar />
       {posts.map((post) => (
         <div key={post.id}>
           <PostCard post={post} />
@@ -53,7 +52,8 @@ export default async function Search({
         <div className="grow text-center py-12">
           <i>Nenhum resultado encontrado.</i>
         </div>
-      )}
+      )} */}
+      {searchParams?.q}
     </>
   );
 }
